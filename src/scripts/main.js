14fdd163ed21a6a528e5dbaa807e2026effc35bb
Michael Phillips <mphillips242001@outlook.com>
2025-05-22 23:03:38 -0500

    makePottery function complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,14 @@
 // Imports go first
-
+import { makePottery } from "./PotteryWheel.js"
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
-
-
+let mug = makePottery("Mug", 2, 5)
+let pot = makePottery("pot", 3, 5)
+let vase = makePottery("vase", 6, 10)
+let jar = makePottery("jar", 6, 8)
+let plate = makePottery("plate", 7, 1)
+console.log(mug, pot, vase, jar, plate)
 // Fire each piece of pottery in the kiln
 
 
